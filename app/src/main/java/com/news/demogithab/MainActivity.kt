9406e817407df6e735154699dbc667bac3ed4f55
a.abdul <ahmedrzr@gmail.com>
2020-10-15 15:23:07 +0300

    first_commit_experimental1

--- app/src/main/java/com/news/demogithab/MainActivity.kt
+++ app/src/main/java/com/news/demogithab/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate: EXPERIMENTAL")
+        Log.d(TAG, "onCreate: EXPERIMENTAL_1")
     }
 }
